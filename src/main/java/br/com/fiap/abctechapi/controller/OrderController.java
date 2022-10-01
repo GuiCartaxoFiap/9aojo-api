d9ecd98ec4067b8e15278a5649f51bc19f41b933
Abraao Neves <6154363+abraaoneves@users.noreply.github.com>
2022-10-01 13:06:26 -0300

    Remove all autowired from construct methods

--- src/main/java/br/com/fiap/abctechapi/controller/OrderController.java
+++ src/main/java/br/com/fiap/abctechapi/controller/OrderController.java
@@ -1,30 +1,25 @@
 package br.com.fiap.abctechapi.controller;
 
-import br.com.fiap.abctechapi.application.OrderApplication;
-import br.com.fiap.abctechapi.application.dto.OrderDto;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.fiap.abctechapi.application.OrderApplication;
+import br.com.fiap.abctechapi.application.dto.OrderDto;
+import lombok.RequiredArgsConstructor;
+
 @RestController
 @RequestMapping("/order")
+@RequiredArgsConstructor
 public class OrderController {
 
-    private OrderApplication orderApplication;
-
-    public OrderController(
-            @Autowired OrderApplication orderApplication
-    ){
-        this.orderApplication = orderApplication;
-    }
+    private final OrderApplication orderApplication;
 
     @PostMapping
-    public ResponseEntity createOrder(@RequestBody OrderDto orderDto) throws Exception {
+    public ResponseEntity<Void> createOrder(@RequestBody OrderDto orderDto) throws Exception {
         orderApplication.createOrder(orderDto);
-
         return ResponseEntity.ok().build();
     }
 }
